4050bb58269f78e377ae29aafa4075cfd3486638
JMLCweb <128441389+JMLCweb@users.noreply.github.com>
2024-10-04 19:08:03 +0100

    create-fe-be-separated-server

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Portfólio</h1>
+      <p>Bem-vindo ao meu site!</p>
+    </div>
+  );
+}
+
+export default App;
